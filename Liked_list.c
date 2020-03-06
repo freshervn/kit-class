d258ca735333970cb894f5849e1b66744723151b
freshervn <40496285+freshervn@users.noreply.github.com>
2020-03-06 13:34:45 +0700

    linked_list(plus and printf)

--- Liked_list.c
+++ Liked_list.c
@@ -4,9 +4,13 @@
 
 typedef struct liked_list
 {
+
     int key;
+
     int data1;
+
     char *data2;
+
     struct liked_list *next_poiter;
 } LinkedList;
 
@@ -14,50 +18,58 @@ LinkedList *Head = NULL;
 
 void Printf(LinkedList *Head);
 void plush(int updateKey, int updateData1, char *updateData2);
-void Find(int key);
 
 int main()
 {
+
     srand(time(0));
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 7; i++)
     {
+
         plush(i, rand() % 100 + 1, "ABCD");
     }
 
     Printf(Head);
+
     return 0;
 }
+
 void plush(int updateKey, int updateData1, char *updateData2)
 {
+
     LinkedList *new_element = malloc(sizeof(LinkedList));
+
     new_element->key = updateKey;
+
     new_element->data1 = updateData1;
+
     new_element->data2 = updateData2;
 
     new_element->next_poiter = Head;
+
     Head = new_element;
 }
-void Printf(LinkedList *Head)
-{
-    LinkedList *new_element = malloc(sizeof(LinkedList));
-    new_element = Head;
-    while (new_element!= NULL);
-    {
-        printf("\n key= %d", new_element->key);
-        printf("\n data1= %d", new_element->data1);
-        printf("\n data2 =%s", new_element->data2);
-        printf("\n");
-        new_element = new_element->next_poiter;
 
-    };
-}
-void Find(int key)
+void Printf(LinkedList *Head)
+
 {
+
     LinkedList *new_element = malloc(sizeof(LinkedList));
+
     new_element = Head;
-    while (new_element!=NULL)
+
+    while (new_element != NULL)
     {
-        /* code */
-    } 
+
+        printf("\n key= %d", new_element->key);
+
+        printf("\n data1= %d", new_element->data1);
+
+        printf("\n data2 =%s", new_element->data2);
+
+        printf("\n");
+
+        new_element = new_element->next_poiter;
+    };
 }
