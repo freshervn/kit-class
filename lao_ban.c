8a009447f21b9c32410492be02c5f0a146de9254
freshervn <40496285+freshervn@users.noreply.github.com>
2020-03-08 20:07:49 +0700

    fix bug

--- lao_ban.c
+++ lao_ban.c
@@ -18,36 +18,42 @@
 
 #include "stdio.h"
 
-int X[7]={2,1,4,5,1,3,3};// X[i]= Y;
+int X[7]={10,2,5,2,10,2,2};// X[i]= Y;
 int length=6;
 int Xmax[100000000]={};
 
 int maxX(int i,int hightbefore);
 
-
 int main()
 {
     int Max=maxX(0,0);
+    // diện tích lớn nhất chứa cạnh đầu tiên. 
+
+    // tính diện tích lớn nhất có thể thu dc từ cách cạnh sau
+  
     for (int i = 1; i <= length; i++)
     {
-        if (X[i] > X[i-1])
+        if (X[i] > X[i-1])   // nếu độ cao cạnh sau nhỏ hoac bằng  cạnh truoc do bỏ qua
         {
         Max= maxX(i,X[i-1]+1) > Max? maxX(i,X[i-1]+1):Max;
         }        
     }
+
     printf("\n%d",Max);
     return 0;
 }
-int maxX(int i,int highbefore)
-{   // find max of  each hight;
+int maxX(int i,int highbefore) 
+{  
+     // find max of  each hight;
     int maxY=X[i];
-    for(int y=highbefore; y <=X[i] ; y++)
+    for(int y=highbefore; y <=X[i] ; y++)//xét từ ô sau cao hơn ô truoc
     {
         int j=i;
-        while ( (X[j]>=y) && (j<=length) )
+        while ((j+1<=length)&&(X[j+1]>=y))// tính chiều ngang lớn nhất có thể thu dc
         {
-            ++j;            
+            j=j+1;            
         }
+        // diện tích hình chữ nhật chiều dài y, chiều rông (j-i+1)
         maxY= (y*(j-i+1) > maxY ? y*(j-i) : maxY );        
     }
     return maxY;
