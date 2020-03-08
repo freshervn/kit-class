b44af4d4e06e8a1f1eddf0df04a80dfbd7d5a3a8
freshervn <40496285+freshervn@users.noreply.github.com>
2020-03-08 15:04:51 +0700

    lão ban

--- test.c
+++ test.c
@@ -1,118 +1,54 @@
+/************************************************************************
+*                  lỗ ban                                               *
+*                                                                       *
+************************************************************************/
+/*
+    y   ^
+        |           000
+        |        000000   
+        |  000   000000   00000
+        |  000   000000   00000
+        |  000   000000   00000
+        |  00000000000000000000
+        |  00000000000000000000 
+        .--------------------------------> 
+                                          X
+*/
+
+
 #include "stdio.h"
-#include "stdlib.h"
-#include "time.h"
 
-typedef struct linked_list
-{
-    int key;
-    int data1;
-    char *data2;
-    struct linked_list *next_poiter;
-} LinkedList;
+int X[7]={2,1,4,5,1,3,3};// X[i]= Y;
+int length=6;
+int Xmax[100000000]={};
 
-LinkedList *Head = NULL;
-
-void Printf(LinkedList *Head);
-void plush( int upateKey, int updateData1, char *updateData2);
-void InSert(LinkedList *Head, int Key, int NewKey, int NewData1, char *NewData2);
-void Delete( int key);
-LinkedList *FindKey(LinkedList *Head, int Key);
+int maxX(int i,int hightbefore);
 
 
 int main()
 {
-
-    srand(time(0));
-    for (int i = 0; i < 5; i++)
+    int Max=maxX(0,0);
+    for (int i = 1; i <= length; i++)
     {
-        plush(i, rand() % 100 + 1, "ABCD");
+        if (X[i] > X[i-1])
+        {
+        Max= maxX(i,X[i-1]+1) > Max? maxX(i,X[i-1]+1):Max;
+        }        
     }
-    Printf(Head);
-    InSert(Head, 3, 9, 100, "new Element");
-    Delete(1);
-    Printf(Head);
+    printf("\n%d",Max);
     return 0;
 }
-
-void plush(int updateKey, int updateData1, char *updateData2)
-{
-
-    LinkedList *new_element = malloc(sizeof(LinkedList));
-    new_element->key = updateKey;
-    new_element->data1 = updateData1;
-    new_element->data2 = updateData2;
-    new_element->next_poiter = Head;
-    Head = new_element;
-}
-
-void Printf(LinkedList *Head)
-{
-    LinkedList *new_element = malloc(sizeof(LinkedList));
-    new_element = Head;
-    while (new_element != NULL)
+int maxX(int i,int highbefore)
+{   // find max of  each hight;
+    int maxY=X[i];
+    for(int y=highbefore; y <=X[i] ; y++)
     {
-
-        printf("\n key= %d", new_element->key);
-        printf("\n data1= %d", new_element->data1);
-        printf("\n data2 =%s", new_element->data2);
-        printf("\n");
-        new_element = new_element->next_poiter;
-    };
-}
-LinkedList *FindKey(LinkedList *Head, int key)
-{
-    LinkedList *newElement = malloc(sizeof(LinkedList));
-    newElement = Head;
-    while (newElement != NULL)
-    {
-        if (newElement->key == key)
+        int j=i;
+        while ( (X[j]>=y) && (j<=length) )
         {
-            return newElement;
+            ++j;            
         }
-        newElement = newElement->next_poiter;
+        maxY= (y*(j-i+1) > maxY ? y*(j-i) : maxY );        
     }
-    return NULL;
-}
-void InSert(LinkedList *Head, int Key, int NewKey, int NewData1, char *NewData2)
-{
-    LinkedList *NewElement = malloc(sizeof(LinkedList));
-    if (FindKey(Head, Key) != NULL)
-    {
-        NewElement->key = NewKey;
-        NewElement->data1 = NewData1;
-        NewElement->data2 = NewData2;
-        NewElement->next_poiter = FindKey(Head, Key)->next_poiter;
-        FindKey(Head, Key)->next_poiter = NewElement;
-    }
-}
-void Delete(int key)
-{
-    LinkedList *newElement,*preElement = malloc(sizeof(LinkedList));
-    newElement = Head;
-    if (newElement->key == key)
-    {
-        Head=newElement->next_poiter;
-        free(newElement);
-        return;
-    }
-    else
-    {
-        while (newElement!= NULL && newElement->key!=key)
-        {
-            preElement=newElement;
-            newElement=newElement->next_poiter;
-        }   
-        if (newElement!=NULL)
-        {
-            preElement->next_poiter=newElement->next_poiter;
-            free(newElement);
-        }
-        else
-        {
-            return ;
-        }
-        
-            
-    }
-
+    return maxY;
 }
