a52ddb289ea2f2b3a9a38fbc70ca470b9aa87abb
freshervn <40496285+freshervn@users.noreply.github.com>
2020-03-16 15:46:35 +0700

    con ngua

--- HORSE.C
+++ HORSE.C
@@ -1,6 +1,5 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include <time.h> //
 typedef struct horse
 {
     int x;
@@ -9,22 +8,30 @@ typedef struct horse
 
 } HORSE; // struct để lưu những tọa độ đã đi qua.
 HORSE *Head = NULL;
-HORSE *way=NULL;
+HORSE *Way=NULL;
 int targetX = 7, targetY = 7;
 int horseX = 0, horseY = 0;
+
 void Plush(int x, int y); // cộng ô đã đi vào danh sách;
-void WayPlush(int x, int y);// gi lại đường đi
-void printfway();// in đường đi
 void Printf(HORSE *taill);
 int  Check(int x,int y);
 int minstep(int numStep,HORSE *Taill);
 
-int main(int argc, char const *argv[])
+void WayPlush(int x, int y);// gi lại đường đi
+int printfway();// in đường đi
+
+int main()
 {
-     srand(time(0)); 
+    printf("nhap toa do con ngua hien tai");
+    scanf("%d %d",&horseX,&horseY);
+    printf("\n nhap toa độ ô tới");
+    scanf("%d %d",&targetX,&targetY);
     
     Plush(horseX,horseY);
+    WayPlush(targetX,targetY);
     minstep(1,NULL);
+    WayPlush(horseX,horseY);
+    printfway();
     return 0;
 }
 void Plush(int x, int y)
@@ -39,8 +46,8 @@ void WayPlush(int x, int y){
     HORSE *newElement = (HORSE *)malloc(1 * sizeof(HORSE));
     newElement->x = x;
     newElement->y = y;
-    newElement->next = way;
-    way = newElement;
+    newElement->next = Way;
+    Way = newElement;
 }
 void Printf(HORSE *taill)
 {
@@ -52,14 +59,17 @@ void Printf(HORSE *taill)
         newElement = newElement->next;
     }
 }
-void printfway(int x, int y){
-     HORSE *newElement = (HORSE *)malloc(sizeof(HORSE));
-    newElement = way;
-    while (newElement != NULL)
+int printfway(){
+    HORSE *newElement = (HORSE *)malloc(sizeof(HORSE));
+    newElement = Way;
+    int i=0;
+    while (newElement!= NULL)
     {
         printf("%d,%d\n", newElement->x, newElement->y);
         newElement = newElement->next;
+        i++;
     }
+    printf("so buoc =%d",i-1);
 }
 int  Check(int X,int Y){
     HORSE * Check_This=(HORSE*)malloc(sizeof(HORSE));
@@ -88,7 +98,16 @@ int minstep(int numStep,HORSE *Taill)
             {
                 if (Element->x+2==targetX&&Element->y+1==targetY)// ô đích
                 {
-                    printf("\n%d",numStep);
+                    targetX=Element->x;
+                    targetY=Element->y;
+                    if (targetX==horseX&&targetY==horseY)
+                    {
+                       return numStep;
+                    }                    
+                    WayPlush(Element->x,Element->y);
+                    Head=NULL;
+                    Plush(horseX,horseY);
+                    minstep(1,NULL);
                     return numStep;
                 }
                 else if (Check(Element->x+2,Element->y+1))// ko phải ô đích và ko phải ô đã đi qua
@@ -100,7 +119,16 @@ int minstep(int numStep,HORSE *Taill)
             {
                 if (Element->x+2==targetX&&Element->y-1==targetY)
                 {
-                    printf("\n%d",numStep);
+                    targetX=Element->x;
+                    targetY=Element->y;
+                     if (targetX==horseX&&targetY==horseY)
+                    {
+                       return numStep;
+                    }  
+                    WayPlush(Element->x,Element->y);
+                    Head=NULL;
+                    Plush(horseX,horseY);
+                    minstep(1,NULL);
                     return numStep;
                 }
                 else if (Check(Element->x+2,Element->y-1))
@@ -115,7 +143,16 @@ int minstep(int numStep,HORSE *Taill)
             {
                 if (Element->x-2==targetX&&Element->y+1==targetY)// ô đích
                 {
-                    printf("\n%d",numStep);
+                    targetX=Element->x;
+                    targetY=Element->y;
+                     if (targetX==horseX&&targetY==horseY)
+                    {
+                         return numStep;
+                    }  
+                    WayPlush(Element->x,Element->y);
+                    Head=NULL;
+                    Plush(horseX,horseY);
+                    minstep(1,NULL);
                     return numStep;
                 }
                 else if (Check(Element->x-2,Element->y+1))// ko phải ô đích
@@ -127,7 +164,17 @@ int minstep(int numStep,HORSE *Taill)
             {
                 if (Element->x-2==targetX&&Element->y-1==targetY)
                 {
-                    printf("\n%d",numStep);
+                    targetX=Element->x;
+                    targetY=Element->y;
+                     if (targetX==horseX&&targetY==horseY)
+                    {
+                         return numStep;
+                    }  
+
+                    WayPlush(Element->x,Element->y);
+                    Head=NULL;
+                    Plush(horseX,horseY);
+                    minstep(1,NULL);
                     return numStep;
                 }
                 else if (Check(Element->x-2,Element->y-1))
@@ -142,7 +189,16 @@ int minstep(int numStep,HORSE *Taill)
             {
                 if ((Element->x+1==targetX)&&(Element->y+2==targetY))
                 {
-                    printf("\n%d",numStep);
+                    targetX=Element->x;
+                    targetY=Element->y;
+                     if (targetX==horseX&&targetY==horseY)
+                    {
+                     return numStep;
+                    }  
+                    WayPlush(Element->x,Element->y);
+                     Head=NULL;
+                    Plush(horseX,horseY);
+                    minstep(1,NULL);
                     return numStep;
                 }
                 else if (Check(Element->x+1,Element->y+2))
@@ -154,7 +210,16 @@ int minstep(int numStep,HORSE *Taill)
             {
                 if ((Element->x-1==targetX)&&(Element->y+2==targetY))
                 {
-                    printf("\n%d",numStep);
+                    targetX=Element->x;
+                    targetY=Element->y;
+                     if (targetX==horseX&&targetY==horseY)
+                    {
+                       return numStep;
+                    }  
+                    WayPlush(Element->x,Element->y);
+                    Head=NULL;
+                    Plush(horseX,horseY);
+                    minstep(1,NULL);
                     return numStep;
                 }
                 else if (Check(Element->x-1,Element->y+2))
@@ -169,7 +234,16 @@ int minstep(int numStep,HORSE *Taill)
             {
                 if ((Element->x+1==targetX)&&(Element->y-2==targetY))
                 {
-                    printf("\n%d",numStep);
+                    targetX=Element->x;
+                    targetY=Element->y;
+                     if (targetX==horseX&&targetY==horseY)
+                    {
+                     return numStep;
+                    }  
+                    WayPlush(Element->x,Element->y);
+                   Head=NULL;
+                    Plush(horseX,horseY);
+                    minstep(1,NULL);
                     return numStep;
                 }
                 else if (Check(Element->x+1,Element->y-2))
@@ -181,7 +255,16 @@ int minstep(int numStep,HORSE *Taill)
             {
                 if ((Element->x-1==targetX)&&(Element->y-2==targetY))
                 {
-                    printf("\n%d",numStep);
+                    targetX=Element->x;
+                    targetY=Element->y;
+                     if (targetX==horseX&&targetY==horseY)
+                    {
+                         return numStep;
+                    }  
+                    WayPlush(Element->x,Element->y);
+                     Head=NULL;
+                    Plush(horseX,horseY);
+                    minstep(1,NULL);
                     return numStep;
                 }
                 else if (Check(Element->x-1,Element->y-2))
@@ -193,8 +276,6 @@ int minstep(int numStep,HORSE *Taill)
         Element=Element->next;
     }
     if(new_taill!=Head){
-    Printf(new_taill);
-    printf("\n");
     minstep(numStep+1,new_taill);
     }
 }
